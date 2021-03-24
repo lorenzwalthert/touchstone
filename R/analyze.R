4591ba28d5041c2b5e7f93f552f437ba2b5d4a1a
Lorenz <lorenz.walthert@icloud.com>
2021-03-25 00:35:25 +0100

    capsule artifact writing in function

---
+++ R/analyze.R
@@ -0,0 +1,44 @@
+#' Turn raw benchmark results into text and figures
+#'
+#' Creates two side effects:
+#'
+#' * Density plots for each element in `refs` are written to `touchstone/plots`.
+#' * A text explaining the speed diff is written to
+#'   `touchstone/pr-comment/info.txt` for every registered benchmarking
+#'   expression.
+#' @export
+benchmarks_analyse <- function(refs = c(
+                                 Sys.getenv("GITHUB_BASE_REF"),
+                                 Sys.getenv("GITHUB_HEAD_REF")
+                               )) {
+  purrr::walk(benchmark_ls(), benchmark_analyze, refs = refs)
+}
+
+
+benchmark_analyze <- function(benchmark, refs) {
+  # TODO test this
+  timings <- benchmark_read(benchmark, refs)
+
+  timings %>%
+    ggplot2::ggplot(ggplot2::aes(x = elapsed, color = ref)) +
+    ggplot2::geom_density()
+  fs::path("touchstone/plots/", benchmark) %>%
+    fs::path_ext_set("png") %>%
+    ggplot2::ggsave()
+
+  tbl <- timings %>%
+    dplyr::group_by(.data$ref) %>%
+    dplyr::summarise(m = mean(.data$elapsed)) %>%
+    tibble::deframe()
+
+  diff_percent <- round(100 * (tbl[refs[2]] - tbl[refs[1]]) / tbl[refs[1]], 1)
+  cat(
+    glue::glue(
+      "{benchmark}: {round(tbl[refs[1]], 2)} -> {round(tbl[refs[2]], 2)} ",
+      "({diff_percent}%)"
+    ),
+    fill = TRUE,
+    file = "touchstone/pr-comment/info.txt",
+    append = TRUE
+  )
+}
