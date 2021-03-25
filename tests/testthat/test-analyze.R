0d7fcb62d203ca70450450280e39e47d839710cb
Lorenz <lorenz.walthert@icloud.com>
2021-03-25 15:14:38 +0100

    add basic analyze test

---
+++ tests/testthat/test-analyze.R
@@ -0,0 +1,19 @@
+test_that("can analyze results", {
+  branch <- gert::git_branch()
+  # generate results
+  local_clean_touchstone()
+  withr::local_options(list(touchstone.skip_install = TRUE))
+  path_test_pkg <- local_package()
+  bm <- benchmark_run_iteration(
+    "",
+    xx1 = "Sys.sleep(runif(1, 0, 1e-5))",
+    ref = branch,
+    n = 5
+  )
+  benchmarks_analyze(branch)
+  expect_match(
+    readLines("touchstone/pr-comment/info.txt"),
+    "xx1: .* -> .* \\(.*%\\)"
+  )
+  expect_true(fs::file_exists("touchstone/plots/xx1.png"))
+})
