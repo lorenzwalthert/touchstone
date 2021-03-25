c3e4407347a9003275e30d49b96cb7fe1c58e928
Lorenz <lorenz.walthert@icloud.com>
2021-03-25 20:16:33 +0100

    uninstall test pkg after test

--- tests/testthat/test-prepare.R
+++ tests/testthat/test-prepare.R
@@ -8,7 +8,6 @@ test_that("can install in isolated repos", {
 
   name_tmp_pkg <- "bli44"
   path_pkg <- local_package(path_temp_pkg(name_tmp_pkg), r_sample = "x <- 55")
-
   lib_path2 <- benchmark_ref_install("devel", path_pkg = path_pkg)
   expect_equal(
     withr::with_libpaths(lib_path1, bli44:::x), 55
