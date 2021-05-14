c0bd9a946f2b840aa989e7a5935c2d83c76450bc
Lorenz Walthert <lorenz.walthert@icloud.com>
2021-05-15 01:45:16 +0200

    add namespace

--- R/analyze.R
+++ R/analyze.R
@@ -77,7 +77,7 @@ confint_relative_get <- function(timings, refs, reference, ci) {
     dplyr::mutate(
       block = factor(.data$block), ref = factor(.data$ref, levels = refs)
     )
-  fit <- aov(elapsed ~ block + ref, data = timings_with_factors)
+  fit <- stats::aov(elapsed ~ block + ref, data = timings_with_factors)
   var <- paste0("ref", refs[2])
   confint <- confint(fit, var, level = ci)
   paste0("[", paste0(set_sign(round(100 * confint / reference, 2)), collapse = "%, "), "%]")
