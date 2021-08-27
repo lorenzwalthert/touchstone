604d28ef0d5e6c07cd85c8b0a14a2694c3b2617b
assignUser <github@krushak.me>
2021-08-27 17:30:30 +0200

    update example script

--- inst/script.R
+++ inst/script.R
@@ -1,10 +1,15 @@
 # see `help(run_script, package = 'touchstone')` on how to run this
 # interactively
 
+
+touchstone::add_lib_dirs("some/dir") #<-- TODO Add directories you want to
+                                     # source in this file or the benchmarks
+
 touchstone::refs_install() # installs branches to benchmark
 
 # benchmark a function call from your package (two calls per branch)
 touchstone::benchmark_run_ref(
+  expr_before_benchmark = source("dir/data.R"), #<-- TODO setup before benchmark
   random_test = yourpkg::f(), #<- TODO put the call you want to benchmark here
   n = 2
 )
