e18f3f74fd48005b9ee3fbdb105c6c94e2fb7159
assignUser <jacob@wujciak.de>
2021-10-22 15:35:41 +0200

    add documentation

--- R/source.R
+++ R/source.R
@@ -11,15 +11,14 @@
 #'   to the library path when the script at `path` is executed, see 'Why this
 #'   function?' below.
 #' @section How to run this interactively?:
+#' You can use [activate()] to setup the envrionment to interactivley run your
+#'  script, as there are some adjustments needed to mirror the Github Action
+#'  environment.
 #' In a GitHub Action workflow, the environment variables `GITHUB_BASE_REF` and
 #' `GITHUB_HEAD_REF` denote the target and source branch of the pull request -
 #' and these are default arguments in [benchmark_run_ref()] (and other functions
 #' you probably want to call in your benchmarking script) to determinate the
-#' branches to use. Hence, you must set these with [base::Sys.setenv()], e.g. if
-#' you want to benchmark the local branch `devel` against `main`
-#' ```
-#' Sys.setenv(GITHUB_BASE_REF = "main", GITHUB_HEAD_REF = "devel")
-#' ```
+#' branches to use.
 #' @section Why this function?:
 #' For isolation, \{touchstone\} does not allow the benchmarked package to be
 #' installed in the global package library, but only in touchstone libraries, as
@@ -59,10 +58,28 @@ run_script <- function(path = "touchstone/script.R",
     "Copied touchstone script to tempdir to prevent branch checkouts to effect",
     " the script."
   ))
-  
+
   source(temp_file, max.deparse.length = Inf, local = TRUE)
 }
 
+#' Activate touchstone environment
+#'
+#' This sets envvars and options to make interactivley running
+#'  [touchstone_script] possible.
+#' @param head_ref Git ref to be used as the HEAD ref when running benchmarks.
+#'  Defaults to the current branch.
+#' @param base_ref Git ref for the baselin ref when running benchmarks. Defaults
+#'  'main' if the option `touchstone.default_base_ref`is not set.
+#' @param env In which environment the temporary changes should be made.
+#'  For use within functions.
+#' @examples
+#' \dontrun{
+#' activate()
+#' # You can now test parts of your touchstone/script.R
+#' deactivate()
+#' }
+#' @export
+#' @md
 activate <- function(head_ref = gert::git_branch(),
                      base_ref = getOption(
                        "touchstone.default_base_ref",
@@ -94,6 +111,8 @@ activate <- function(head_ref = gert::git_branch(),
   }
 }
 
+#' @describeIn activate Restore the original environment state.
+#' @export
 deactivate <- function(env = parent.frame()) {
   withr::deferred_clear(envir = env)
   cli::cli_alert_success("Original environment restored!")
